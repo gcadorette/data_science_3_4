80ce46d1d38f02642236880b696ea3c85be2e8e2
gcadorette <cadg3202@usherbrooke.ca>
2020-12-02 03:45:50 -0500

    added data;)

---
+++ main.py
@@ -0,0 +1,13 @@
+# Guillaume Cadorette - cadg3202 - 17 040 202
+
+DATA_FILE_PATH = "data/u.item"
+
+
+class Film:
+    def __init__(self, year, genre):
+        self.year = year
+        self.genre = genre
+
+
+if __name__ == "__main__":
+    print("hey :)")
