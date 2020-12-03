70fd391743294b8d2b3ced2e55d32c8539978160
gcadorette <cadg3202@usherbrooke.ca>
2020-12-03 06:15:41 -0500

    num1 pas fini smh

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ DATA_FILE_PATH = "data/u.item"
 USER_FILE_PATH = "data/u.user"
 BASE_FILE_PATH = "data/u_.base"
 TEST_FILE_PATH = "data/u_.test"
-EPSILON = 0.01  # seuil de convergence
+EPSILON = 0.66  # seuil de convergence
 
 
 class Rating:
@@ -30,76 +30,101 @@ class User:
 """
 
 
-def kmeans(amtOfClusters, values):
+def kmeans(amtOfClusters, values, usersInFile, filmsInFile):
     clusters = []
     clustersMid = []
-    for ind in range(amtOfClusters + 1):
-        clustersMid.append(values.keys()[ind])
-        clusters.append([])
-    for _ in amtOfClusters:
-        for key, value in values.items():
-            pass
-            # TODO: FAIRE LE ACTUAL KMEANS LMAO
-            # EN GROS ON COMPARE LES DISTANCES SUR LES FILMS QUE LES DEUX ONT RATE
-            # FACILE FACILE CITRON PRESSE
-
-
-
-"""
-def distance(rating1: Rating, rating2: Rating):
-    ageDiff = abs(rating1.user.age - rating2.user.age) ** 2
-    sexDiff = abs(rating1.user.sex - rating2.user.sex) ** 2
-    jobDiff = 1 if rating1.user.job != rating2.user.job else 0
-
-    yearDiff = abs(rating1.film.year - rating2.film.year) ** 2
-    genreDiff = 0
-    for index in len(rating1.film.genre):
-        genreDiff += 1 if rating1.film.genre[index] != rating2.film.genre[index] else 0
-    genreDiff = genreDiff ** 2
-    ratingDiff = abs(rating1.rating - rating2.rating) ** 2
-
-    return ratingDiff, genreDiff, yearDiff, jobDiff, sexDiff, ageDiff
-"""
-
-def distance(rating1: Rating, rating2: Rating):
-
-
-
-def distanceRating(rating1, rating2):
-    pass
-
-
-def distanceUser(user1, user2):
-    ageDiff = abs(user1.age - user2.age) ** 2
-    sexDiff = abs(user1.sex - user2.sex) ** 2
-    jobDiff = 1 if user1.job != user2.job else 0
-    return [ageDiff, sexDiff, jobDiff]
+    for ind in range(amtOfClusters):
+        clustersMid.append(usersInFile[25*ind])
+        clusters.append([usersInFile[25*ind]])
+    midValues = []
+    for ind in range(amtOfClusters):
+        midValues.append([x for x in values if x.user == clustersMid[ind]])
+    changed = True
+    amtOfIterations = 0
+    while changed:
+        changed = False
+        for user in usersInFile:
+            minDiff = 2**31
+            chosenCluster = -1
+            for clusterInd in range(amtOfClusters):
+                if clustersMid[clusterInd] != user and user not in clustersMid:
+                    diff = 0
+                    amt = 0
+                    ind = 0
+                    for j, x in enumerate(values):
+                        if x.user == user:
+                            ind = j
+                            break
+                    while ind < len(values) and values[ind].user == user:
+                        midRatings = [x for x in midValues[clusterInd] if x.film == values[ind].film]
+                        for val in midRatings:
+                            diff += abs(values[ind].rating - val.rating) / len(midRatings)
+                            amt += 1
+                        ind += 1
+                    if amt > 0:
+                        #diff = diff / len(midValues[clusterInd])
+                        if diff < minDiff:
+                            chosenCluster = clusterInd
+                            minDiff = diff
+            for cl in range(amtOfClusters):
+                if user in clusters[cl]:
+                    clusters[cl].remove(user)
+            clusters[chosenCluster].append(user)
+        amtChanged = 0
+        for clusterInd in range(amtOfClusters):
+            prevMid = clustersMid[clusterInd]
+            sumVal = 0
+            amtMovies = 0
+            for film in filmsInFile:
+                filmRatings = [x.rating for x in values if x.film == film and x.user in clusters[clusterInd]]
+                if filmRatings:
+                    sumVal += sum(filmRatings) / len(filmRatings)
+                    amtMovies += 1
+                    pass
+            avg = sumVal / amtMovies
+            ratingsMid = [x.rating for x in values if x.user == clustersMid[clusterInd]]
+            closestMid = abs(avg - sum(ratingsMid)) / len(ratingsMid)
+            bestMid = clustersMid[clusterInd]
+            mins = []
+            for user in clusters[clusterInd]:
+                filmRatings = [x.rating for x in values if x.user == user]
+                rating = sum(filmRatings) / len(filmRatings)
+                if abs(abs(avg - rating) - closestMid) > EPSILON:
+                    mins.append(abs(abs(avg - rating) - closestMid))
+                    closestMid = abs(avg - rating)
+                    bestMid = user
+                    amtChanged += 1
+            if bestMid != prevMid:
+                print("Cluster : {}".format(clusterInd))
+                print(mins)
+                midValues[clusterInd] = [x for x in values if x.user == clustersMid[clusterInd]]
+                clustersMid[clusterInd] = bestMid
+                changed = True
+                for cl in range(amtOfClusters):
+                    if bestMid in clusters[cl]:
+                        clusters[cl].remove(bestMid)
+                clusters[clusterInd].append(bestMid)
+        print(amtOfIterations)
+        amtOfIterations += 1
+        print("lol")
+    print("hihi")
 
 
 if __name__ == "__main__":
-    """
-    films = {}
-    users = {}
-    with open(DATA_FILE_PATH) as f:
-        for line in f:
-            entry = line.replace("\n", "").split("|")
-            films[entry[0]] = Film(entry[2].split("-")[-1], "".join(entry[5:]).replace("|", ""))
-    with open(USER_FILE_PATH) as f:
-        for line in f:
-            entry = line.replace("\n", "").split("|")
-            users[entry[0]] = User(entry[0], entry[1], entry[2], entry[3])
-    """
+
     ratings = []
     for i in range(1, 6):
         usersInFile = []
         filmsInFile = []
         with open(BASE_FILE_PATH.replace("_", str(i))) as f:
-            ratings.append({})
+            ratings.append([])
             for line in f:
                 entry = line.split("\t")
-                # ratings[-1].append(Rating(users[entry[0]], films[entry[1]], entry[2]))
-                # ratings[-1].append(entry[0], entry[1], entry[2])
-                ratings[-1][entry[0]] = Rating(entry[0], entry[1], entry[2])
-        for k in range(2, 13):
-            kmeans(k, ratings[-1])
+                ratings[-1].append(Rating(entry[0], entry[1], entry[2]))
+                if int(entry[0]) not in usersInFile:
+                    usersInFile.append(int(entry[0]))
+                if int(entry[1]) not in filmsInFile:
+                    filmsInFile.append(int(entry[1]))
+        for k in range(5, 9):
+            kmeans(k, ratings[-1], usersInFile, filmsInFile)
     print("me:)")
